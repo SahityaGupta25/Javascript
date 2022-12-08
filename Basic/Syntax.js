e9634002405b548f28885fe6290fe5db10ca2253
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-08 17:05:08 +0530

    real

---
+++ Basic/Syntax.js
@@ -0,0 +1,45 @@
+//How to Create Variable
+
+let a = 9
+var b = 'Mathew'
+
+//Their is One more way to Create Variable
+
+const c = null
+
+// But we Only use this when we want the value to be permanent for whole program. 
+
+// How to Acces a Variable
+
+console.log(` This is Value of a ${a}
+This is Value of ${b}
+    This is Value of ${c}`)
+
+
+//How to use '+' Operator with Variable
+
+console.log(`My Age is ${a} year old and my name is ${b}`)
+
+//How to Calculate Length of a String
+
+console.log("The Length of b is ="+b.length)
+
+
+// Indirect a String is an Array. You can Access elements using []
+
+console.log("The Third Element is = "+b[3])
+
+// Addition
+
+// 1st Way
+
+console.log("5"+5+8)
+// Whenever their is a String type value it will Concatinate . Not add the numbers
+
+// 2nd way
+
+console.log(5+5)
+// Now you can see that the answer is in number because their is string type stuff in the print statement
+
+// 3rd way
+console.log(+"7"+"5"+5)
