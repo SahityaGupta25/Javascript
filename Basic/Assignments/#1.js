2f7d02fef59e65e073bc28f466d5abaf169847ab
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-20 16:29:26 +0530

    assignments

---
+++ Basic/Assignments/#1.js
@@ -0,0 +1,14 @@
+let firstName="sahitya";
+let lastName='gupta'
+let age=21
+let country='India'
+let city='NCR'
+let year=1999
+let ismarried=false
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(age));
+console.log(typeof(country));
+console.log(typeof(city));
+console.log(typeof(year));
+console.log(typeof(ismarried));
