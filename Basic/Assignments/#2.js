108015db7bce71ba1bdb6e7bbcf03e2b31a1d46c
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-21 19:04:42 +0530

    2 & 3

---
+++ Basic/Assignments/#2.js
@@ -0,0 +1,23 @@
+// 2. Boolean value is either true or false.
+//--------------------------------------------------------------------
+// a. Write three JavaScript statements which provide truthy value.
+a=5
+b='5'
+c=7
+d=8
+
+console.log(a==b);
+console.log(c>a);
+console.log(d!=c);
+
+// b. Write three JavaScript statements which provide falsy value.
+
+a=5
+b='5'
+c=7
+d=8
+
+console.log(a===b);
+console.log(c<a);
+console.log(d==c);
+
