1cef00056c22d88c0d895279fb3dec014a65402e
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-21 19:06:02 +0530

    8 & 9

---
+++ Basic/Assignments/#8.js
@@ -0,0 +1,7 @@
+// 8. Suppose 1 dollar is equal to 82.23 Indian rupee, create a program which converts
+// dollars to rupees.
+
+const rs=82.23
+const dollar=42
+let x=dollar * rs
+console.log(x);
---
+++ Basic/Assignments/#9.js
@@ -0,0 +1,12 @@
+// 9. Write a program to print unit digit of a given number.
+// Input : 6693
+// Output : 3
+
+const x=6693
+let y= x%10
+console.log(y);
+
+
+
+
+
