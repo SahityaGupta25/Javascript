a7670cb98076f69a991a534d08271bf342c03b6d
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-22 15:31:30 +0530

    date

--- Basic/Assignments/#7.js
+++ Basic/Assignments/#7.js
@@ -1,10 +1,12 @@
 // 7. Create a human readable time format using the Date time object
 // a. YYYY-MM-DD HH:mm
+const date = new Date();
+console.log(date.getFullYear()+'-'+date.getMonth()+'-'+date.getDay(),date.getHours()+':'+date.getMinutes());
+
 // b. DD-MM-YYYY HH:mm
+
+
+
 // c. DD/MM/YYYY HH:mm
 
 
-const a=new Date(2023,03,21,4,42)
-const b=new Date(21,03,2023,4,45)
-const c=new Date()
-console.log(b);
