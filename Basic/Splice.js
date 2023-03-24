f7d2b0e53a209e6f2614a60035f82981ff33adea
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-25 04:46:26 +0530

    Methods

---
+++ Basic/Splice.js
@@ -0,0 +1,6 @@
+let names = ['Anurag','Ravi','Vasu','Ram','Krishna']
+names.splice(0,1,'Radhe-Radhe');
+// Both 0 & 1 are Inclusive . 
+console.log(names);
+
+// If you write nothing in place of Appending area . Splice will remove elements .
---
+++ Basic/Split.js
@@ -0,0 +1,6 @@
+const x = 'Mathew'
+console.log(x.split(''));
+
+
+const y = 'google,facebook,amazon,netflix';
+console.log(y.split(','));
