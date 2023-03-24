641b2fe35f054409d8e04f2eb4bd508e919f9e8d
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-24 18:54:31 +0530

    js

---
+++ Basic/Concatinate.js
@@ -0,0 +1,5 @@
+let names = ['Anurag','Ravi','Vasu','Ram','Krishna']
+let count = [66,93,25,16]
+let moto = ['s1000rr','r1','r6','fireblade']
+let newarray = names.concat(count,moto);
+console.log(newarray);
---
+++ Basic/Copywithin.js
@@ -0,0 +1,3 @@
+let count = [66,93,25,16,29,02]
+console.log(count.copyWithin(1,2,5));
+// (Inplace of Index no. , Inclusive from where you want to copy , Exclusive)
