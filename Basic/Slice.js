431f7bcd9bc05dc4d04916b9133fe6595a19503e
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-25 04:45:52 +0530

    Method

---
+++ Basic/Slice.js
@@ -0,0 +1,4 @@
+let names = ['Anurag','Ravi','Vasu','Ram','Krishna']
+
+console.log(names.slice(1,3));
+                    // Inclusive , Exclusive
---
+++ Basic/Sort.js
@@ -0,0 +1,2 @@
+const x = [34,69,16,25,36,49]
+console.log(x.sort());
