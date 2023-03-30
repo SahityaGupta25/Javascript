f47249878133ec09405099f37583d08bfff91e1d
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-30 08:27:01 +0530

    4

---
+++ Assignment2/#4.js
@@ -0,0 +1,6 @@
+// 4. Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+const sen=['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM','Oracle'];
+const tence='and Amazon are big IT companies.';
+let x = sen.join(tence)
+console.log(x);
