54559c3974d252b2bf67c084ba794224be634412
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-03-30 08:27:38 +0530

    8th

---
+++ Assignment2/#8.js
@@ -0,0 +1,12 @@
+// 8. This is a fruit array , ['banana', 'orange', 'mango', 'lemon'] reverse the order using
+// loop without using a reverse method.
+
+
+fruit = ['banana', 'orange', 'mango', 'lemon'];
+newfruit=[]
+for (let i = 2; i <= fruit.length; i--) {
+    x = newfruit.push([i]);
+    
+    
+}
+console.log(newfruit);
