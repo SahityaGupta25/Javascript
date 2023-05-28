d9cce03f6992d52780dc8515ed47fac608ffedba
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-05-28 16:03:18 +0530

    practice_set-3

--- Practice_Set_03.js
+++ Practice_Set_03.js
@@ -10,5 +10,15 @@ for (let i=0 ; i<Object.keys(marks).length ; i++){
 //     marks = {harry:98,yash:40,rohan:12}
     marks = { harry: 98, yash: 40, rohan: 12 }
 for (let key in marks){
-    console.log('The Marks of ' + key + 'is = ' + marks[i]);
+    console.log('The Marks of ' + key + ' is = ' + marks[key]);
 }
+
+// 3 . Write a Program which prints "Try Again " until the user Prints Correct Number.
+
+let cn=5
+let n
+while (n!=cn) {
+    console.log(`Try Again`);
+    n = prompt('Enter Correct Number');
+}
+console.log('Correct!')
