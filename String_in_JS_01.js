84d614181b62d547746d3676ad693a163daed1d3
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-06-20 14:44:19 +0530

    string

---
+++ String_in_JS_01.js
@@ -0,0 +1,24 @@
+let x = 'ram'
+console.log(x)
+
+// If you want to check length of a string we can use length which is pre-defined function.
+
+console.log(x.length) 
+
+// If you want to fetch the first index no. then use []
+
+console.log(x[0])
+
+// Template Literal
+let boy1 = 'Sagar'
+let bike = 'Ktm'
+console.log(`${boy1} rides ${bike}`)
+
+
+
+// Escape Sequences Character
+let fruit = "app\'le"
+console.log(fruit)
+'\n' = "new line"
+'\t' = "tab"
+'\r' = "Carriage Return"
