78598da3fb25a57c64a3ee878a2054b7b80a4663
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-06-25 23:00:28 +0530

    6

--- Practice_Set_06.js
+++ Practice_Set_06.js
@@ -13,14 +13,25 @@ else{
 
 // 3. Use Console.error in first question and log error if age entered in -ve.
 
-let a = prompt("Enter your Age")
-Number.parseInt(a)
+let b = prompt("Enter your Age")
+Number.parseInt(b)
 
-if (a >= 18) {
+if (b >= 18) {
   alert("You can Drive");
 }
-else if (a < 0) {
+else if (b < 0) {
   console.error("Invalid Age")
 } else {
   alert("You can't drive");
 }
+
+
+// 4 . If User Enters a Number greater than 5 than Re-direct him/her to replit.com
+
+let c = prompt("Enter a Number")
+Number.parseInt(c)
+if (c>5){
+  location.href = "https://replit.com"
+}
+
+//   5. Change Background colour according to the input of a user by prompt.
