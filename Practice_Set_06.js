ec691c213d4dc657f8826aba1f6aa971c0f53d64
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-06-25 22:32:41 +0530

    6

--- Practice_Set_06.js
+++ Practice_Set_06.js
@@ -8,3 +8,19 @@ if (a>=18){
 else{
   alert("You can't drive");
 }
+
+// 2. In Q1 use confirm to ask user if he wants to see prompt again.
+
+// 3. Use Console.error in first question and log error if age entered in -ve.
+
+let a = prompt("Enter your Age")
+Number.parseInt(a)
+
+if (a >= 18) {
+  alert("You can Drive");
+}
+else if (a < 0) {
+  console.error("Invalid Age")
+} else {
+  alert("You can't drive");
+}
