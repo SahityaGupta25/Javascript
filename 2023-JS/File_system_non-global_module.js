e5df897515a94ed0ca723216eae00614824b1d12
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-10-15 21:27:20 +0530

    IMP F

--- 2023-JS/File_system_non-global_module.js
+++ 2023-JS/File_system_non-global_module.js
@@ -1,2 +1,13 @@
-// File System is a non global module which means we must import those modules before use 
+// File System is Core Module in Node JS a non global module which means we must import those modules before use 
+//  Global - COnsole.log , filename etc
+//  LOcal - Buffer , file system etc
 
+
+let fs = require('fs');
+console.log("directory",__dirname);
+console.log(`filename`,__filename);
+
+
+// .writeFileSync is used to create new files in the same module.
+let fs = require('fs').writeFileSync;
+fs("abc.txt",'xyz');
