5abdcbf6ad5cf0a3e89ff9acca91a0cf50bc7de0
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-10-15 17:37:50 +0530

    files

--- 2023-JS/Array_filter.js
+++ 2023-JS/Array_filter.js
@@ -1,6 +1,15 @@
-// Filter
-let arr = [3,56,67,87,90];
-arr.filter((data)=>{
-    console.log(data);
+// Filter Note: Filter functions work's as a loop.
+let arr = [3,56,67,87,9,3,0];
+let a1= arr.filter((data)=>{
+    // console.log(data);
+    // It will print elements which are greater than 3 
+//    return data>3
+
+// It will print elements which are equal 3 
+   return data===3
+
+    
+    
 })
 // FIlter always works on array only 
+console.log(a1);
---
+++ 2023-JS/File_system_non-global_module.js
@@ -0,0 +1,2 @@
+// File System is a non global module which means we must import those modules before use 
+
