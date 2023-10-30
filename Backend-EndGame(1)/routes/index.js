a6e9dc03bba1276fa5e54b0b021924ccf5b7322a
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2023-10-30 11:34:27 +0530

    index

--- Backend-EndGame(1)/routes/index.js
+++ Backend-EndGame(1)/routes/index.js
@@ -3,16 +3,31 @@ var router = express.Router();
 const userModel = require('./users')
 
 /* GET home page. */
-router.get('/',function(req,res){
-  res.render("index")
-})
+// router.get('/',function(req,res){
+//   res.render("index")
+// })
 
 router.get  ('/create', async function(req,res){
-  await userModel.create({
+  // * This create method will return user thats why I kept it in variable
+  const createUser = await userModel.create({
     username:'Mathew',
     age:16,
     name:'Ujjwal'
   })
+  res.send(createUser);
+})
+
+router.get  ('/allusers', async function(req,res){
+  // * This create method will return user thats why I kept it in variable
+  const allUSers = await userModel.find()
+    res.send(allUSers)
+  
+})
+router.get  ('/user', async function(req,res){
+  // * This create method will return user thats why I kept it in variable
+  const singleUser = await userModel.findOne({username:"Mathew"})
+    res.send(singleUser)
+  
 })
 
 module.exports = router;
