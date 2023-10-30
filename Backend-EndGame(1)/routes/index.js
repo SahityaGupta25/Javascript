18c4db4119b1ab88f111e1ace3cf56b5f466e6c3
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2023-10-30 11:15:34 +0530

    index

--- Backend-EndGame(1)/routes/index.js
+++ Backend-EndGame(1)/routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const
+const userModel = require('./users')
 
 /* GET home page. */
 router.get('/',function(req,res){
   res.render("index")
+})
+
+router.get  ('/create', async function(req,res){
+  await userModel.create({
+    username:'Mathew',
+    age:16,
+    name:'Ujjwal'
+  })
 })
 
 module.exports = router;
