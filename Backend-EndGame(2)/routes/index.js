928d2874315a2c5688c237f6975c7e02f08ca959
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2023-11-07 19:19:17 +0530

    users

--- Backend-EndGame(2)/routes/index.js
+++ Backend-EndGame(2)/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require('./users')
+const userModel = require('./users');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,13 +11,19 @@ router.get('/', function(req, res) {
 
 router.get('/create', async function(req, res) {
   const userData = await userModel.create({
-      username:"Mathew",
-      nickname:"Ujjwal",
-      description:"I am Rich",
-      categories:["Healthy","Wealthy","Kind","Generous","Tall","Responsible"]
+      username:"Hanuman",
+      nickname:"Balaji",
+      description:"I am Devotee of Lord Ram ",
+      categories:["Avtar of Shiv","8-Sidhhis"]
     })
     res.send(userData)
 });
+
+router.get('/find', async function(req,res){
+  var regex = new RegExp("^krIshna$",'i')
+  let user = await userModel.find({username:regex})
+  res.send(user)
+})
 
 
 
