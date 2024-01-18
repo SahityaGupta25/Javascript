8e1ec6f024e10ff0e3e4dc11f55ca7917cceaa0f
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-01-18 12:46:15 +0530

    function

--- 2023-JS/Function_syntax.js
+++ 2023-JS/Function_syntax.js
@@ -23,4 +23,19 @@
 // console.log(addno(25,66,93,16,2,29));
 
 
+// Another Approach
 
+
+
+function addno(...ans){
+    let sum = 0
+    let i;
+    for (i = 0 ; i<arguments.length;i+=1){
+
+        sum = arguments[i]+sum;
+    }
+    return sum
+    }
+
+    let ans = addno(25,66,93,11,2,29)
+    console.log(ans);
