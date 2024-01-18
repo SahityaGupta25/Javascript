ad51ec0f0438ec84dfa1004d6a879657f7ba5c08
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-01-18 12:42:15 +0530

    updated

--- 2023-JS/Function_syntax.js
+++ 2023-JS/Function_syntax.js
@@ -1,7 +1,26 @@
-function multply(a,b){
+// function multply(a,b){
 
-    console.log(a*b);
-}
+//     console.log(a*b);
+// }
 
 
-multply(5,25)
+// multply(5,25)
+
+
+// Q. Add unlimited numbers which are given in parameter
+
+// function addno(){
+//     let sum = 0
+//     let i;
+//     for (i = 0 ; i<arguments.length;i+=1){
+
+//         sum = arguments[i]+sum;
+//     }
+//     return sum
+//     }
+    
+//     The arguments is an object which is local to a function.
+// console.log(addno(25,66,93,16,2,29));
+
+
+
