e13729b4e27bcf0cd02fc5fdd900138fb94b390d
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-10 13:02:14 +0530

    script

--- Learn JS with 4 projects/Quiz Game/script.js
+++ Learn JS with 4 projects/Quiz Game/script.js
@@ -1,11 +1,20 @@
 const prompt = require("prompt-sync")()
 console.log("Welcome to the QUIZ GAME!");
-let value = prompt("What is the brain of Computer")
+let value = prompt("What is the brain of Computer  ")
 console.log(value);
-if (value === 'CPU') {
+if (value.toUpperCase() === 'CPU') {
     console.log("You are Correct");
 }
 else{
     console.log("You are incorrect");
+}
+
+let value2= prompt("Which one is better 1050ti or 1650ti  ")
+let answer2 = "1650ti"
+if (value2.toLowerCase() === answer2 ) {
+    console.log("Correct 1650ti is better");
+} else {
+    console.log("Incorrect , 1650ti is better");
+    
 }
 
