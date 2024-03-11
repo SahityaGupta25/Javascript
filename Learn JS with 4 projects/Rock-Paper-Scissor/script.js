0e23953cabc64bafcb8e73e23a74c7ed68b6a62e
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-11 14:43:26 +0530

    svript

--- Learn JS with 4 projects/Rock-Paper-Scissor/script.js
+++ Learn JS with 4 projects/Rock-Paper-Scissor/script.js
@@ -1,2 +1,26 @@
-const prompt = require('prompt-sync')()
+const prompt = require("prompt-sync")()
 
+let win =0 
+let loss = 0
+let ties = 0 
+let playerChoice = prompt("Rock or Paper or Scissors : ").toLowerCase()
+
+    if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice  !== "scissors") {
+        console.log("Invalid Choice");
+    }
+    if(playerChoice === computerchoice){
+        console.log("Draw!");
+        ties++;
+    }
+    else if ((playerChoice === "paper" && computerchoice=== "rock") || (playerChoice === "rock" && computerchoice=== "scissors") || (playerChoice === "scissors" && computerchoice=== "paper")){
+            console.log("You won!");
+            win++;
+    }
+    else {
+        console.log("You lose");
+        loss++;
+    }
+const choices = ["rock","paper","scissors"]
+const randomIndex = Math.round(Math.random()*2);
+const computerchoice = choices[randomIndex]
+console.log(computerchoice); 
