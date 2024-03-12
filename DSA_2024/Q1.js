2b60b61f9b13c46c225889cfe57d8ce3c4e759c3
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-12 10:45:56 +0530

    question

--- DSA_2024/Q1.js
+++ DSA_2024/Q1.js
@@ -1,1 +1,16 @@
 // Sum of Natural Number from 1 to N .
+
+
+
+function natural(n) {
+    
+    let sum =0
+    for (let i = 1; i < n+1; i++) {
+        sum=sum+i;
+        
+    }
+    
+    return sum;
+}
+
+console.log(natural(3)); 
