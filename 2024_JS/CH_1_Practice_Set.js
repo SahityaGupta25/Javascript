ab989ec763c052bbf66e088e625689b864c24d64
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-04 10:24:54 +0530

    practice

--- 2024_JS/CH_1_Practice_Set.js
+++ 2024_JS/CH_1_Practice_Set.js
@@ -3,4 +3,33 @@
 let x = "Mathew "
 let y = 66
 let z = x+String(y)
-console.log(z);
+//! console.log(z);
+console.log(x+y);
+
+// * Use typeof operator to find the datatype of the string in last question.
+//! console.log(typeof z);
+
+// * create a const object in JS . Can you change it to hold a number later?
+
+const person = {
+    name:"Mathew",
+    age : 16,
+    gender : "Male",
+
+
+}
+
+person = 25
+//! console.log(person);
+
+// * Try to add a new key to the const object in Problem 3 were you able to do it.
+
+// * Write a JS program to create a word-meaning dictionary of 5 words.
+
+let dict = {
+    deliberately : "Knowingly",
+    diligently : "joy",
+    inadvertently : "Unknowingly",
+    hassleFree : "with out efforts",
+    cakeWalf : "very easy"
+}
