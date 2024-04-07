f66521f44d7c276f51cd1ee55f5bcb9faf7c5048
SahityaGupta25 <vasuguptaracer@gmail.com>
2024-04-07 17:14:01 +0530

    updated

---
+++ 2024_JS/Projects/Guess Number/script.js
@@ -0,0 +1,7 @@
+const randomNumber = parseInt(Math.random() *100+1)
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSLot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+console.log(remaining,userInput);
