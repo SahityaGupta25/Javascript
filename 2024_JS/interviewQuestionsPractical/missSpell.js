9bad8cc9ea6d398843cc9cae8128e9e27a845c3c
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-11 16:29:01 +0530

    missspell

---
+++ 2024_JS/interviewQuestionsPractical/missSpell.js
@@ -0,0 +1,3 @@
+let message;
+mesge = {data: 12}//!       JS will automatic declare a undeclare variable to the window scope but if we "use strict" then JS will not declare by itself.
+console.log(mesge);
