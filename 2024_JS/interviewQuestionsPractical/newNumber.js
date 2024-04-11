2f878304cc84dfd0697a348307b06a3bfc6ccd07
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-11 16:03:40 +0530

    number

---
+++ 2024_JS/interviewQuestionsPractical/newNumber.js
@@ -0,0 +1,9 @@
+let a = 108
+let b = new Number(108)//!       new Number() will keep 108 as a value in a oject 
+let c = 108
+
+console.log(a==b);
+console.log(a===b);
+console.log(b==c);
+
+console.log(b);
