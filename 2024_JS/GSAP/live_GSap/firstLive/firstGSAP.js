00db96abf25ce7f1c0384cbd4bae9469f452a470
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-17 08:26:05 +0530

    files

--- 2024_JS/GSAP/live_GSap/firstLive/firstGSAP.js
+++ 2024_JS/GSAP/live_GSap/firstLive/firstGSAP.js
@@ -2,7 +2,58 @@ gsap.to("#box",{
     x: 700,
     duration: 2,
     delay: 2,
-    backgroundColor:"yellow",
+    backgroundColor:"green",
+    color: "white",
     rotate:180,
     scale: 0.6
 })
+// gsap.from("#box",{
+//     x: 700,
+//     duration: 2,
+//     delay: 2,
+//     backgroundColor:"yellow",
+//     rotate:180,
+//     scale: 0.6
+// })
+
+gsap.to("h1",{
+    x: 500,
+    y: 500,
+    duration: 2,
+    delay: 1,
+    backgroundColor: "yellow",
+    color: "black"
+
+})
+
+
+
+gsap.to("hr",{
+    duration: 0.5,
+    delay: 0.2,
+    color: "black"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
