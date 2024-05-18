1be7b7fc81a2f2c7b1f61f1f44b68985c51e01d7
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-05-18 18:04:45 +0530

    views

--- 2024_JS/backendDev/Task 2/app.js
+++ 2024_JS/backendDev/Task 2/app.js
@@ -3,10 +3,11 @@ const app = express()
 
 app.set("view engine","ejs")
 app.use(express.json());
-app.use(express.urlencoded{extende: true})
+app.use(express.urlencoded({extende: true}))
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/',(req,res) => {
-res.send('Hello')
+res.render("index")
 })
 
+app.listen(3000)
