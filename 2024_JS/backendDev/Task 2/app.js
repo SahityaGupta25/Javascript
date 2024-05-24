75ad5b4fc5bb2ae42242ee2321d710c7ee0f6852
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-05-24 18:51:23 +0530

    files

--- 2024_JS/backendDev/Task 2/app.js
+++ 2024_JS/backendDev/Task 2/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const userModel = require('./models/user')
 
 app.set("view engine","ejs")
 app.use(express.json());
@@ -9,6 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',(req,res) => {
 res.render("index")
+})
+app.get('/read',(req,res) => {
+res.render("read")
 })
 app.get('/read',(req,res) => {
 res.render("read")
