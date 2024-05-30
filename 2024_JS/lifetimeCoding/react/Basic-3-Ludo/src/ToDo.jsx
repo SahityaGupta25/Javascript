4633058bd9a46e49a11e34a0ab22b1f93039f10e
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-05-30 14:32:31 +0530

    src

--- 2024_JS/lifetimeCoding/react/Basic-3-Ludo/src/ToDo.jsx
+++ 2024_JS/lifetimeCoding/react/Basic-3-Ludo/src/ToDo.jsx
@@ -6,17 +6,21 @@ const ToDo = () => {
     let [todos,setTodods]= useState(["Sample Tasks.........."])
     let [newTodo,setNewTodo] = useState("")
     function addTask(){
-        setTodods(...todos)
+        setTodods([...todos,newTodo])
+        setNewTodo("")
+    }
+    let updateValue = (event)=>{
+        setNewTodo(event.target.value)
     }
   return (
     <div className='todo'>
-      <input type="text" placeholder='Add a Task'></input>
-      <button>Add Task</button>
+      <input type="text" value={newTodo} onChange={updateValue} placeholder='Add a Task'></input>
+      <button onClick={addTask}>Add Task</button>
       <h3>TO DO Task</h3>
       <ul>
-        {todos.map((todo)=>{
+        {todos.map((todo)=>(
             <li>{todo}</li>
-        })}
+        ))}
       </ul>
     </div>
   )
