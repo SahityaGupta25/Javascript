a7e53a833f48f5bf24665eb9832866ea605d23cd
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-06-09 20:43:33 +0530

    php

---
+++ 2024_JS/cwh/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello
+    <?php
+   $variable1 = "Gopal";
+   $variable2 = 25;
+
+   echo $variable1;
+   echo "<br>";
+   echo $variable2;
+   ?>
+</body>
+</html>
