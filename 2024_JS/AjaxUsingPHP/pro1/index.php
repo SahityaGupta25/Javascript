5cd0d3d78be463c5ffcf342af5fd366828b154a6
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-06-10 09:24:30 +0530

    index

--- 2024_JS/AjaxUsingPHP/pro1/index.php
+++ 2024_JS/AjaxUsingPHP/pro1/index.php
@@ -8,19 +8,34 @@
 </head>
 <style>
     html,body{
-        
+
     }
     .container{
         max-width: 80%;
         background-color: grey;
         margin: auto;
+        padding: 25px;
     }
 </style>
 <body>
     <div class="container">
     <h1>Lets Learn about PHP</h1>
     <?php 
+    $age = 25;
+    if($age == 25){
+echo "You are Eligible";
+    }
+   elseif ($age < 25) {
+    echo "You are Junior";
+   }
+    # code...
+   
+    else{
+echo "You are senior and can't fit";
+    }
 
+    $arr1 = [93,26,66,51,11,21,29];
+    echo $arr1[3];
      ?>
     </div>
 </body>
