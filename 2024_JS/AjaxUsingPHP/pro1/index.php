b30bc6302aac2fdf968a2bf35b38b75830e1a5d6
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-06-10 08:36:52 +0530

    pro1

---
+++ 2024_JS/AjaxUsingPHP/pro1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<style>
+    html,body{
+        
+    }
+    .container{
+        max-width: 80%;
+        background-color: grey;
+        margin: auto;
+    }
+</style>
+<body>
+    <div class="container">
+    <h1>Lets Learn about PHP</h1>
+    <?php 
+
+     ?>
+    </div>
+</body>
+</html>
